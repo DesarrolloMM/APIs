6242a58b8a5fde53fec480d2ab906c824893520c
Matias <matias.mrz51@gmail.com>
2022-06-15 19:05:46 -0300

    Added user class

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+// Configure the HTTP request pipeline.
+
+
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
